ac967a95b30f5023f63d2a051d0dc8150f1ff777
example <example@example.com>
2023-02-08 16:55:15 +0700

    update chat screen script

--- client/src/pages/Test/Test.js
+++ client/src/pages/Test/Test.js
@@ -1,11 +1,10 @@
 import React from "react";
 import Layout from "../../components/Layout/Layout";
-import CreateGroup from "../../components/CreateGroupModal/useCreateGroupModal";
 
 export default function Test() {
   return (
     <Layout>
-      <CreateGroup />
+      test page
     </Layout>
   );
 }
